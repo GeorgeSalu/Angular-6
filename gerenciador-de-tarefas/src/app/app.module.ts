5a4ccd687e23301856564b458e5684360201f9ec
George Salu <george.salu10@gmail.com>
2018-06-09 16:38:23 -0300

    implementando a directiva de tarefa concluida

--- gerenciador-de-tarefas/src/app/app.module.ts
+++ gerenciador-de-tarefas/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TarefasModule } from './tarefas';
-import { TarefaConcluidaDirective } from './tarefas/shared/tarefa-concluida.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TarefaConcluidaDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
