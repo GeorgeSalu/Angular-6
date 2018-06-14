b1e770338ed3a49c5ace86e5799a149293892149
George Salu <george.salu10@gmail.com>
2018-06-13 21:58:17 -0300

    criando o servico de gerencimanto de moedas

--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConversorComponent } from './components';
+import { MoedaService } from './services';
 
 @NgModule({
   imports: [
@@ -9,6 +10,9 @@ import { ConversorComponent } from './components';
   declarations: [ConversorComponent],
   exports: [
   	ConversorComponent
+  ],
+  providers: [
+  	MoedaService
   ]
 })
 export class ConversorModule { }
