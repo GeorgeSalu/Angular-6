d1c9b8ac67afa6342b81ff4f5f22b26d320c6817
George Salu <george.salu10@gmail.com>
2018-06-14 21:59:49 -0300

    implementando a diretiva de validação de numeros 1 - 2

--- conversor-de-moedas/src/app/app.module.ts
+++ conversor-de-moedas/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ConversorModule } from './conversor';
 import { HttpClientModule } from '@angular/common/http';
-import { NumeroDirective } from './conversor/directives/numero.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NumeroDirective
   ],
   imports: [
     BrowserModule,
