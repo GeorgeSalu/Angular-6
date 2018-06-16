80bb732959a94221b864f94366a6e52c46c55279
George Salu <george.salu10@gmail.com>
2018-06-16 14:42:15 -0300

    ciclo de vida de um componente

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { PhotoService } from './photos/photo/photo.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   photos: Object[] = [];
 
-  constructor(photoservice: PhotoService) {
-    photoservice
-        .listFromUser('flavio')
-        .subscribe(photos => this.photos = photos);
+  constructor(private photoservice: PhotoService) {
   }
+
+  ngOnInit(): void {
+    this.photoservice
+          .listFromUser('flavio')
+          .subscribe(photos => this.photos = photos);
+  }
+
 }
