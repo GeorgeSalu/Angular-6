a2fb8e6e3277e2fb6d186b72a21120cd5926de34
George Salu <george.salu10@gmail.com>
2018-06-16 11:53:23 -0300

    criando o primeiro componente

---
+++ alurapic/src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'ap-photo',
+	templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+
+    title = 'Leao';
+
+    url = 'http://pt.kabbalah.com/sites/dev02.kce.kabbalahcentres.com/files/14-07-18-leo-a-double-edged-sword.jpg'
+
+}
