a840015e199ca92f5ef5ad9755b1d1d808fbcc6c
George Salu <george.salu10@gmail.com>
2018-06-21 15:31:21 -0300

    BrowserModule e CommonModule

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -2,10 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoComponent } from './photo/photo.component';
 import { NgModule } from '@angular/core';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ PhotoComponent, PhotoListComponent ],
 
-  imports: [HttpClientModule]
+  imports: [HttpClientModule, CommonModule]
 })
 export class PhotosModule {}
