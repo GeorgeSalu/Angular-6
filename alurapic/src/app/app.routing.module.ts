fb509f538388f2f2cc9222aef24078de3e3b431e
George Salu <george.salu10@gmail.com>
2018-06-23 10:27:10 -0300

    lidando com rotas inexistentes

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes:Routes  = [
   { path: 'user/flavio', component: PhotoListComponent },
-  { path: 'p/add', component: PhotoFormComponent }
+  { path: 'p/add', component: PhotoFormComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
