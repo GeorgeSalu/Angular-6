2b9887f4fde3ff1dc49c6874d45d40da6f684ece
George Salu <george.salu10@gmail.com>
2018-06-22 22:48:29 -0300

    configuranco rota do modulo de login

--- ponto-inteligente/src/app/autenticacao/login/login.module.ts
+++ ponto-inteligente/src/app/autenticacao/login/login.module.ts
@@ -1,11 +1,14 @@
+import { LogarComponent } from './components/logar.component';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, LogarComponent]
 })
 export class LoginModule { }
