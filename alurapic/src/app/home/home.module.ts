4a4d49c93bd4775cd4075726faca1d4f9a658e08
George Salu <george.salu10@gmail.com>
2018-07-01 17:35:15 -0300

    Validação de formularios

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { SignInComponent } from './signin/signin.component';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ SignInComponent ]
+  declarations: [ SignInComponent ],
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class HomeModule {}
