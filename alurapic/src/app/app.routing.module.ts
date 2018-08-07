38cae4437067899dc7d2c4d893825f46b52f0be8
George Salu <george.salu10@gmail.com>
2018-08-06 22:44:34 -0300

    implementando o lazy loading

--- alurapic/src/app/app.routing.module.ts
+++ alurapic/src/app/app.routing.module.ts
@@ -1,29 +1,19 @@
-import { HomeComponent } from './home/home.component';
-import { AuthGuard } from './core/auth/auth.guard';
-import { SignInComponent } from './home/signin/signin.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
-import { SingUpComponent } from './home/singup/singup.component';
 
 const routes:Routes  = [
   {
     path: '',
-    component: HomeComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: '',
-        component: SignInComponent
-      },
-      {
-        path: 'singup',
-        component: SingUpComponent
-      },
-    ]
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomeModule'
   },
 
   {
--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { HomeRoutingModule } from './home.routing.module';
 import { SingUpComponent } from './singup/singup.component';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from './../shared/components/vmessage/vmessage.module';
@@ -9,6 +10,6 @@ import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [ SignInComponent, SingUpComponent, HomeComponent ],
-  imports: [ReactiveFormsModule, FormsModule ,CommonModule, VMessageModule, RouterModule]
+  imports: [ReactiveFormsModule, FormsModule ,CommonModule, VMessageModule, RouterModule, HomeRoutingModule]
 })
 export class HomeModule {}
