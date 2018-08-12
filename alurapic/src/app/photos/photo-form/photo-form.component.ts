3b9d6549eb6990433bfb10dfd0d4a1d8378a0470
George Salu <george.salu10@gmail.com>
2018-08-12 15:24:03 -0300

    VAlidando o formulario

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { AuthService } from "../../core/auth/auth.service";
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { Validators } from "@angular/forms";
 
 @Component({
-  selector: 'ap-photo-form',
-  templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+    selector: 'ap-photo-form',
+    templateUrl: './photo-form.component.html',
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+    photoForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private formBuilder: FormBuilder) { }
 
+    ngOnInit(): void {
+
+        this.photoForm = this.formBuilder.group({
+            file: ['', Validators.required],
+            description: ['', Validators.maxLength(300)],
+            allowComments: [true]
+        });
+    }
 }
