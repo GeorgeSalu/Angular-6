9ff7a783f8e377634160dc43d2b9b14c4dd2c9d4
George Salu <george.salu10@gmail.com>
2018-08-12 19:47:00 -0300

    seleção automatica de arquivos

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -5,10 +5,19 @@ import { PhotoFormComponent } from './photo-form.component';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VMessageModule } from '../../shared/components/vmessage/vmessage.module';
+import { ImmediateClickModule } from '../../shared/directive/immediate-click/immediate-click.module';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, VMessageModule, FormsModule, RouterModule, PhotoModule]
+  imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      VMessageModule,
+      FormsModule,
+      RouterModule,
+      PhotoModule,
+      ImmediateClickModule
+  ]
 })
 export class PhotoFormModule {
 
