c57b225979e6b95e13deae861ca7f09dcb644873
George Salu <george.salu10@gmail.com>
2018-08-12 21:21:36 -0300

    Buscando a foto selecionada da API

--- alurapic/src/app/photos/photo-details/photo-details.component.ts
+++ alurapic/src/app/photos/photo-details/photo-details.component.ts
@@ -1,16 +1,22 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './photo-details.component.html'
 })
 export class PhotoDetailsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  photo$: Observable<Photo>;
+  constructor(private route: ActivatedRoute, private photoService: PhotoService) { }
 
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params.photoId;
+    this.photo$ = this.photoService.findById(
+      this.route.snapshot.params.photoId
+    )
   }
 
 }
