7486e41bf9f129c52f504fc9e22d159e3e34173f
George Salu <george.salu10@gmail.com>
2018-08-15 21:06:00 -0300

    comentando fotos

--- alurapic/src/app/photos/photo-details/photo-comments/photo.comments.component.ts
+++ alurapic/src/app/photos/photo-details/photo-comments/photo.comments.component.ts
@@ -27,4 +27,13 @@ export class PhotoCommentsComponent implements OnInit{
     })
   }
 
+  save() {
+    const comment = this.commentForm.get('comment').value as string;
+    this.photoService
+        .addComment(this.photoId, comment)
+        .subscribe(() => {
+          this.commentForm.reset();
+        })
+  }
+
 }
--- alurapic/src/app/photos/photo/photo.service.ts
+++ alurapic/src/app/photos/photo/photo.service.ts
@@ -40,7 +40,11 @@ export class PhotoService {
   }
 
   getComment(photoId: number) {
-    return this.http.get<PhotoComment[]>(API + '/photos/'+photoId+'/comments')
+    return this.http.get<PhotoComment[]>(API + '/photos/'+photoId+'/comments');
+  }
+
+  addComment(photoId: number, commentText: string) {
+    return this.http.post(API + '/photos/'+photoId+'/comments', { commentText });
   }
 
 }
