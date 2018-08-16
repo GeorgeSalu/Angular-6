28be244de219b418c6f7ace61c5fabd38d957fec
George Salu <george.salu10@gmail.com>
2018-08-15 21:15:45 -0300

    Lidando com o refresh de comentarios

--- alurapic/src/app/photos/photo-details/photo-comments/photo.comments.component.ts
+++ alurapic/src/app/photos/photo-details/photo-comments/photo.comments.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
 
+import { switchMap, tap } from 'rxjs/operators';
+
 @Component({
   selector: 'ap-photo-comments',
   templateUrl: './photo.comments.component.html'
@@ -29,11 +31,12 @@ export class PhotoCommentsComponent implements OnInit{
 
   save() {
     const comment = this.commentForm.get('comment').value as string;
-    this.photoService
+    this.comments$ = this.photoService
         .addComment(this.photoId, comment)
-        .subscribe(() => {
+        .pipe(switchMap(() => this.photoService.getComment(this.photoId)))
+        .pipe(tap(() => {
           this.commentForm.reset();
-        })
+        } ))
   }
 
 }
